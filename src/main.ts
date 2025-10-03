9b08629e99cd47a3ca07faac918d64ae1794abb9
rcplaza <code.rcplaza@gmail.com>
2025-10-02 23:51:49 -0300

    feat: add welcome view, add copy text files, add asset pikachu

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { router } from "@/router";
+import "@/style.css";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import { createApp } from "vue";
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,7 +1,67 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from "@/components/button/Button.vue";
+import { copy } from "@/copy/en";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const goToPokemonList = (): void => {
+  router.push({ name: "list" });
+};
+</script>
 
 <template>
-  <h1>Welcome</h1>
+  <main class="main">
+    <img class="logo" src="@/assets/pikachu.svg" alt="Pikachu" />
+
+    <div class="view__text">
+      <h1 class="title">{{ copy.welcome.title }}</h1>
+
+      <p class="caption">{{ copy.welcome.caption }}</p>
+    </div>
+    <Button class="btn__start" @click="goToPokemonList">
+      <template #label>
+        <span>{{ copy.buttons.getStarted }}</span>
+      </template>
+    </Button>
+  </main>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.main {
+  align-items: center;
+  background-color: var(--color-background);
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-lg);
+  min-height: 100dvh;
+  padding: var(--space-lg) var(--space-xs) var(--space-xs) var(--space-xs);
+  width: 100%;
+}
+
+.view__text {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-sm);
+}
+
+.title {
+  font-size: var(--font-size-lg);
+  font-weight: var(--font-weight-bold);
+}
+
+.caption {
+  color: var(--color-text-secondary);
+  font-size: var(--font-size-sm);
+  font-weight: var(--font-weight-regular);
+  max-width: 315px;
+  text-align: center;
+}
+
+@media screen and (min-width: 1024px) {
+  .caption {
+    max-width: 570px;
+  }
+}
+</style>
